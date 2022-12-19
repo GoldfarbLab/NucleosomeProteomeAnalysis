b7f8252c7c8929b59db5cf180a463827a96974d1
a8nguyen <huyenanhnguyen.1234@gmail.com>
2022-12-19 11:37:23 -0600

    fileupload: percolator and rts for other exp

--- preprocess_rts.R
+++ preprocess_rts.R
@@ -1,4 +1,3 @@
-library(dplyr)
 library(tidyverse)
 library(TMTPurityCorrection)
 library(MSstatsHelper)
@@ -14,19 +13,24 @@ if (length(args)==0) {
   stop("2 arguments required. Specify which experiment to plot and output directory", call.=FALSE)
 }
 
-experiments_typs <- c(
-  
+experiment_types <- c(
+  "grouping_all" = file.path("data", "grouping1_firstAttempt"),
+  "v1" = file.path("data", "v1"),
+  "walk" = file.path("data", "walk"),
+  "ap" = file.path("data", "ap")
 )
 
 
 out_dir_path<-args[2]
-impurity_path <- "data/WB314414_2.csv"
-search_path <- "data/grouping1_firstAttempt/merged.tsv"
-inten_path  <- "data/grouping1_firstAttempt/intensity.txt"
-noise_path <- "data/grouping1_firstAttempt/noise.txt"
-gene_path <- "data/gene_list_proteome.tsv"
-percolator_path <- "data/grouping1_firstAttempt/percolator.target.psms.txt"
-annot_path <-"data/grouping1_firstAttempt/annotations.csv"
+input_dir_path <- experiment_types[args[1]]
+input_dir_path <- experiment_types["v1"]
+impurity_path <- file.path("data","WB314414_2.csv")
+search_path <- file.path(input_dir_path, "merged.tsv")
+inten_path  <- file.path(input_dir_path, "intensity.txt")
+noise_path <- file.path(input_dir_path, "noise.txt")
+gene_path <- file.path("data", "gene_list_proteome.tsv")
+percolator_path <- file.path(input_dir_path, "percolator.target.psms.txt")
+annot_path <-file.path(input_dir_path, "annotations.csv")
 
 ### all df
 impurities_df<- read.csv(impurity_path)
@@ -36,22 +40,37 @@ geneName_df <- read_tsv(gene_path)
 geneName_df <- geneName_df %>% dplyr::rename(ProteinName = Entry, GeneName = `Gene names`)
 search_df <- read_tsv(search_path)
 
-names(search_df)[1] <- "Raw file"
-search_df <- search_df %>% dplyr::rename(`Scan number`=`Scan Number`)
-msms <- search_df %>% right_join(intensities_df, by=c("Scan number", "Raw file")) #CHANGE THIS IF MERGED THEN RAW FILE SHOULD BE ON THERE
 
+if ("NAME" %in% names(search_df)){
+  names(search_df)[1] <- "Raw file"
+}
+search_df <- search_df %>% dplyr::rename(`Scan number`=`Scan Number`)
+if ("Raw file" %in% names(search_df)){
+  msms <- search_df %>% right_join(intensities_df, by=c("Scan number", "Raw file")) #CHANGE THIS IF MERGED THEN RAW FILE SHOULD BE ON THERE
+} else {
+  msms <- search_df %>% right_join(intensities_df, by=c("Scan number"))
+}
 corrected_df <- correctImpurities.msms(msms, impurities_df, intensities_df, noise_df, remove.missing.rows = F)
 percolator_df <- read_tsv(percolator_path)
 annot_df <- read.csv(annot_path, header = TRUE)
 
 ### filter round 1: no CON, no REV, signal to noise >= 50, has bridge
-filtered_df <- corrected_df %>% 
-  filter(across(everything(), ~ !grepl("CON_", .))) %>% 
-  filter(across(everything(), ~ !grepl("REV_", .))) %>% 
-  mutate(SN_sum = rowSums(.[grep("SN \\d+", names(.))], na.rm = TRUE )) %>% 
-  filter(SN_sum >= 50) %>%
-  filter(`Is missing 0` == 0) #WATCH OUT READ THE FILTERING CRITERIA!!! 
 
+if ("NAME" %in% names(search_df)){
+  filtered_df <- corrected_df %>% 
+    filter(across(everything(), ~ !grepl("CON_", .))) %>% 
+    filter(across(everything(), ~ !grepl("REV_", .))) %>% 
+    mutate(SN_sum = rowSums(.[grep("SN \\d+", names(.))], na.rm = TRUE )) %>% 
+    filter(SN_sum >= 50) %>%
+    filter(`Is missing 0` == 0) #WATCH OUT READ THE FILTERING CRITERIA!!!
+} else {
+  filtered_df <- corrected_df %>% 
+    filter(across(everything(), ~ !grepl("CON_", .))) %>% 
+    filter(across(everything(), ~ !grepl("REV_", .))) %>% 
+    mutate(SN_sum = rowSums(.[grep("SN \\d+", names(.))], na.rm = TRUE )) %>% 
+    filter(SN_sum >= 50)
+  
+}
 
 ### filter in percolator search q <= 0.01
 percolator_filtered <- percolator_df %>% 
@@ -66,13 +85,21 @@ percolator_filtered$Peptide <- gsub("\\[[^)]*]", "", percolator_filtered$Peptide
 
 ### filter round 2: tryptic sequence reformat
 filtered_df_rd2 <- transform(filtered_df, Peptide=str_sub(Peptide,3,-3))
-runs <- unique(filtered_df_rd2[["Raw.file"]])     #run1 = 1, run2=3, run3= 0, run4=2
-idex <- as.double(c(1,3,0,2))
-filtered_df_rd2$Raw.file <- mapvalues(filtered_df_rd2$Raw.file, from=runs, to=idex)
+if ("NAME" %in% names(search_df)){
+  runs <- unique(filtered_df_rd2[["Raw.file"]])     #run1 = 1, run2=3, run3= 0, run4=2
+  idex <- as.double(c(1,3,0,2))
+  
+  filtered_df_rd2$Raw.file <- mapvalues(filtered_df_rd2$Raw.file, from=runs, to=idex)
+}
+
 filtered_df_rd2 <- mutate(filtered_df_rd2, Raw.file = as.numeric(Raw.file))
 filtered_df_rd2$Peptide <- gsub("\\[[^)]*]", "", filtered_df_rd2$Peptide)
 
-filtered_df_final <- merge(filtered_df_rd2, percolator_filtered, by=c("Scan.number", "Peptide", "Charge.State","Raw.file")) #sometimes need to add Raw.file here
+if ("NAME" %in% names(search_df)){
+  filtered_df_final <- merge(filtered_df_rd2, percolator_filtered, by=c("Scan.number", "Peptide", "Charge.State","Raw.file"))
+} else {
+  filtered_df_final <- merge(filtered_df_rd2, percolator_filtered, by=c("Scan.number", "Peptide", "Charge.State"))
+}#sometimes need to add Raw.file here
 filtered_df_final$Peptide <- gsub("\\[[^)]*]", "", filtered_df_final$Peptide)
 
 
@@ -123,35 +150,63 @@ filtered_df_final_expanded <- filtered_df_final_expanded %>%
 #   dplyr::rename(ProteinName=ProteinName.x)
 
 #psms msstats
-annot.maxquant <- annot_df %>% filter(grepl('Bridge', BioReplicate) ) %>% 
-  mutate(BioReplicate= str_c(Mixture, '_', 'Norm' ) ) %>% 
-  full_join(annot_df) %>% 
-  filter(!grepl('Bridge', BioReplicate)) %>%
-  mutate(Condition = replace(Condition, Condition == "Bridge", "Norm"))
-filtered_df_final_expanded$Raw.file <- mapvalues(filtered_df_final_expanded$Raw.file, from=idex, to=runs)
-
-msstats_tmt_df <- filtered_df_final_expanded %>%
-  select( ProteinName, Peptide, Charge.State, matches("Reporter.intensity.corrected"), 
-          GeneName, Scan.number, Raw.file, 
-          PSM, unique.peptide.per.protein, peptide.per.protein,
-          psm.per.protein, number.of.scans, number.of.unique.peptides, `percolator q-value`) %>% #sometimes we need to add raw.file
-  pivot_longer(cols=matches("Reporter.intensity.corrected"), names_to='Channel', values_to='Intensity') %>%
-  mutate(Channel = str_replace(Channel,"Reporter.intensity.corrected.", "channel.")) %>%
-  dplyr::rename(PeptideSequence = Peptide, Charge=Charge.State, Run = Raw.file) %>% #SOmetimes we have raw file here
-  merge(annot.maxquant, by = c('Channel', "Run"))  %>%  #sometimes we need run here 
-  dplyr::group_by(ProteinName) %>%
-  dplyr::mutate(psm_1 = data.table::uniqueN(PSM[Mixture == 1]),psm_2 = data.table::uniqueN(PSM[Mixture == 2]),
-                psm_3 = data.table::uniqueN(PSM[Mixture == 3]),psm_4 = data.table::uniqueN(PSM[Mixture == 4]),
-                pep_1 = data.table::uniqueN(PeptideSequence[Mixture == 1]),pep_2 = data.table::uniqueN(PeptideSequence[Mixture == 2]),
-                pep_3 = data.table::uniqueN(PeptideSequence[Mixture == 3]),pep_4 = data.table::uniqueN(PeptideSequence[Mixture == 4])) %>% 
-  dplyr::mutate(psm.per.mixture = paste(psm_1,psm_2,psm_3,psm_4, sep = ";"),
-                peptides.per.mixture = paste(pep_1,pep_2,pep_3,pep_4,sep = ";")) %>% 
-  dplyr::mutate(`percolator q-value` = round(`percolator q-value`, 5))
-  select(-psm_1,-psm_2,-psm_3,-psm_4,-pep_1,-pep_2,-pep_3, -pep_4)
+if ("NAME" %in% names(search_df)){
+  annot.maxquant <- annot_df %>% filter(grepl('Bridge', BioReplicate) ) %>% 
+    mutate(BioReplicate= str_c(Mixture, '_', 'Norm' ) ) %>% 
+    full_join(annot_df) %>% 
+    filter(!grepl('Bridge', BioReplicate)) %>%
+    mutate(Condition = replace(Condition, Condition == "Bridge", "Norm"))
+    filtered_df_final_expanded$Raw.file <- mapvalues(filtered_df_final_expanded$Raw.file, from=idex, to=runs)
   
+} else{
+  annot.maxquant <- annot_df %>% filter(grepl('Bridge', BioReplicate) ) %>% 
+    full_join(annot_df) %>% 
+    filter(!grepl('Bridge', BioReplicate)) %>%
+    mutate(Condition = replace(Condition, Condition == "Bridge", "Norm"))
+  
+}
+
+if ("NAME" %in% names(search_df)){
+  msstats_tmt_df <- filtered_df_final_expanded %>%
+    select( ProteinName, Peptide, Charge.State, matches("Reporter.intensity.corrected"), 
+            GeneName, Scan.number, Raw.file, 
+            PSM, unique.peptide.per.protein, peptide.per.protein,
+            psm.per.protein, number.of.scans, number.of.unique.peptides, `percolator q-value`) %>% #sometimes we need to add raw.file
+    pivot_longer(cols=matches("Reporter.intensity.corrected"), names_to='Channel', values_to='Intensity') %>%
+    mutate(Channel = str_replace(Channel,"Reporter.intensity.corrected.", "channel.")) %>%
+    dplyr::rename(PeptideSequence = Peptide, Charge=Charge.State, Run = Raw.file) %>% #SOmetimes we have raw file here
+    merge(annot.maxquant, by = c('Channel', "Run"))  %>%  #sometimes we need run here 
+    dplyr::group_by(ProteinName) %>%
+    dplyr::mutate(psm_1 = data.table::uniqueN(PSM[Mixture == 1]),psm_2 = data.table::uniqueN(PSM[Mixture == 2]),
+                  psm_3 = data.table::uniqueN(PSM[Mixture == 3]),psm_4 = data.table::uniqueN(PSM[Mixture == 4]),
+                  pep_1 = data.table::uniqueN(PeptideSequence[Mixture == 1]),pep_2 = data.table::uniqueN(PeptideSequence[Mixture == 2]),
+                  pep_3 = data.table::uniqueN(PeptideSequence[Mixture == 3]),pep_4 = data.table::uniqueN(PeptideSequence[Mixture == 4])) %>% 
+    dplyr::mutate(psm.per.mixture = paste(psm_1,psm_2,psm_3,psm_4, sep = ";"),
+                  peptides.per.mixture = paste(pep_1,pep_2,pep_3,pep_4,sep = ";")) %>% 
+    dplyr::mutate(`percolator q-value` = round(`percolator q-value`, 5)) %>% 
+    select(-psm_1,-psm_2,-psm_3,-psm_4,-pep_1,-pep_2,-pep_3, -pep_4)
+
 
   
   #select(-Scan.number)
+} else{
+  msstats_tmt_df <- filtered_df_final_expanded %>%
+    select( ProteinName, Peptide, Charge.State, matches("Reporter.intensity.corrected"), 
+            GeneName, Scan.number, 
+            PSM, unique.peptide.per.protein, peptide.per.protein,
+            psm.per.protein, number.of.scans, number.of.unique.peptides, `percolator q-value`) %>% #sometimes we need to add raw.file
+    pivot_longer(cols=matches("Reporter.intensity.corrected"), names_to='Channel', values_to='Intensity') %>%
+    mutate(Channel = str_replace(Channel,"Reporter.intensity.corrected.", "channel.")) %>%
+    dplyr::group_by(ProteinName) %>%
+    dplyr::mutate(psm_1 = data.table::uniqueN(PSM[Mixture == 1]),psm_2 = data.table::uniqueN(PSM[Mixture == 2]),
+                  psm_3 = data.table::uniqueN(PSM[Mixture == 3]),psm_4 = data.table::uniqueN(PSM[Mixture == 4]),
+                  pep_1 = data.table::uniqueN(PeptideSequence[Mixture == 1]),pep_2 = data.table::uniqueN(PeptideSequence[Mixture == 2]),
+                  pep_3 = data.table::uniqueN(PeptideSequence[Mixture == 3]),pep_4 = data.table::uniqueN(PeptideSequence[Mixture == 4])) %>% 
+    dplyr::mutate(psm.per.mixture = paste(psm_1,psm_2,psm_3,psm_4, sep = ";"),
+                  peptides.per.mixture = paste(pep_1,pep_2,pep_3,pep_4,sep = ";")) %>% 
+    dplyr::mutate(`percolator q-value` = round(`percolator q-value`, 5)) %>% 
+    select(-psm_1,-psm_2,-psm_3,-psm_4,-pep_1,-pep_2,-pep_3, -pep_4)
+}
 
 msstats_tmt_df2 <- msstats_tmt_df %>% 
   dplyr::mutate(seq1 = case_when(Mixture ==1 ~ paste0(PeptideSequence, "(", `percolator q-value`, ")")),
