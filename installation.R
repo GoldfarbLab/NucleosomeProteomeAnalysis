c243ca5497fe38d02c302993fb94cdaaab2dedd4
a8nguyen <huyenanhnguyen.1234@gmail.com>
2022-12-20 17:04:39 -0600

    add bioconductor and devtools deps

--- installation.R
+++ installation.R
@@ -1,12 +1,13 @@
 
 packages <- c("tidyverse", "plyr", "stringr", "MSstatsTMT", "ggfortify", "broom","ggthemes","ggrepel",
               "data.table", "factoextra", "cluster", "MSstats", "EnhancedVolcano", "UpsetR", "ComplexHeatmap",
-              "conclust","ComplexHeatmap", "matrixStats", "circlize", "cluster", "RColorBrewer", "ggpubr")
+              "conclust","ComplexHeatmap", "matrixStats", "circlize", "cluster", "RColorBrewer", "ggpubr",
+              "devtools", "BiocManager")
 
 installed_packages <- packages %in% rownames(installed.packages())
 if (any(installed_packages == FALSE)) {
   install.packages(packages[!installed_packages])
 }
-library(devtools)
-install_github("GoldfarbLab/TMTPurityCorrection")
-install_github("GoldfarbLab/MSstatsHelper")
+devtools::install_github("GoldfarbLab/TMTPurityCorrection")
+devtools::install_github("GoldfarbLab/MSstatsHelper")
+BiocManager::install("MSstatsTMT")
